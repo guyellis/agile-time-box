7a5d515c6b5d672665b9d16dca658b3db847a092
Guy Ellis <wildfiction@gmail.com>
2014-09-17 19:38:33 -0700

    Added sounds and .wav files.

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
 "use strict";
 
 var moment = require('moment');
+var player = require('play-sound')({});
 
 function systemBell(times) {
-	times = times > 0 ? times : 1;
-	while(times--) {
-		console.log('\u0007');
-	}
+  player.play('../sounds/sfx/intro.wav');
+//  play.sound('./sounds/sfx/alarm.wav');
+//  play.sound('./sounds/sfx/crinkle.wav');
+//  play.sound('./sounds/sfx/flush.wav');
+//  play.sound('./sounds/sfx/ding.wav');
 }
 
 function padLeadingZeros(textToPad, finalLengthOfText) {
@@ -37,7 +39,7 @@ function main() {
 	}
 	var subsequentTimes = (args.length > 1) ? args[1] : firstTime;
 
-	var alarmTime = moment().add('minutes', firstTime);
+	var alarmTime = moment().add(firstTime, 'minutes');
 
 	console.log('Alarm set to ' + firstTime + ' minute(s) and then every ' + subsequentTimes + ' minute(s) after that.');
 	console.log('First alarm at ' + moment(alarmTime).format('MM/DD/YYYY h:mm:ss'));
@@ -46,7 +48,7 @@ function main() {
 		if(moment() >= alarmTime) {
 			console.log('Time Box complete at ' + moment().format('h:mm:ss'));
 			systemBell(1);
-			alarmTime = moment().add('minutes', subsequentTimes);
+			alarmTime = moment().add(subsequentTimes, 'minutes');
 			console.log('Next alarm at ' + moment(alarmTime).format('MM/DD/YYYY h:mm:ss'));
 		}
 		console.log('Time left: ' +
